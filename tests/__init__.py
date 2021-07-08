708e4c3389d7ad31f7eb5a4de996185ac9787b3c
Florian Briegel <wasndas@zone42.de>
2021-07-08 11:34:32 +0200

    add dummy __init__.py to make pytest happy

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# @Author: Florian Briegel (briegel@mpia.de)
+# @Date: 2021-06-15
+# @Filename: __init__.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+# just to make pytest happy
