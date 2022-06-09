fdaea8652ea68d360d2f656a8fd6a0e65302a4a1
Florian Briegel <briegel@lisa.mpia-hd.mpg.de>
2022-06-09 14:38:01 +0200

    fixed support kubernetes

--- container/run-basdard.sh
+++ container/run-basdard.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/bash -l
 
-BASDARD_NAME=`basename ${BASDARD_CONFIG##*/} .conf`
-BASDARD_PORT=`shuf -i 2000-65000 -n 1`
-BASDARD_ADAPTER="[NAME=$BASDARD_NAME,PORT=$BASDARD_PORT]"
+BASDARD_NAME = `basename ${BASDARD_CONFIG##*/} .conf`
+test -n "$BASDARD_PORT" && BASDARD_PORT = `shuf -i 2000-65000 -n 1`
+BASDARD_ADAPTER = "[NAME=$BASDARD_NAME,PORT=$BASDARD_PORT]"
 
 LVMT_PATH=/root/lvmt
 PATH=$LVMT_PATH/scripts:$PATH
+
+if [ $RMQ_HOST ]; then 
+     RMQ_CONNECTION = "[user=guest,password=guest,host=$RMQ_HOST,port=5672]"
+else
+     test -n "$RMQ_CONNECTION" && RMQ_CONNECTION = "[user=guest,password=guest,host=localhost,port=5672]"
+fi
 
 
 INSROOT_ETC_PATH=$LVMT_PATH/config:$INSROOT_ETC_PATH
@@ -22,6 +28,6 @@ echo $BASDARD_UI
 
 test -n "$DISPLAY" && (sleep 0.3 && niceQUI --XXX.CONFIG:Endpoint=$BASDARD_ADAPTER$BASDARD_UI --LOGGER.LEVEL=INFO) &
 
-basdard --CONFIG=$BASDARD_CONFIG --ADAPTER=$BASDARD_ADAPTER --LOGGER.LEVEL=INFO
+basdard --CONFIG=$BASDARD_CONFIG --ADAPTER=$BASDARD_ADAPTER --CLU.RABBITMQ.CONN:MapStringString=$RMQ_CONNECTION --LOGGER.LEVEL=INFO
 
 
