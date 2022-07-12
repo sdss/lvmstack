8b5eaf2846470e7178ce670aae0df2b5a559fd40
Florian Briegel <wasndas@zone42.de>
2022-07-12 10:48:35 -0700

    updated kube config (partial)

--- config/container/actor/run-actor.sh
+++ config/container/actor/run-actor.sh
@@ -10,33 +10,33 @@ PYTHON=/usr/bin/python3
 
 LVM_ROOT=$HOME
 
-if [ $LVM_DEBUG ]; then 
-  LVM_ACTOR_PATH=$(ls -1 -d ${LVM_ROOT}/lvm/${LVM_ACTOR} ${LVM_ACTOR} 2> /dev/null)/python/${LVM_ACTOR}
+if [ ${LVM_DEBUG} ]; then 
+  LVM_ACTOR_PATH=$(ls -1 -d ${LVM_ROOT}/lvm/${LVM_ACTOR} ${LVM_ROOT}/${LVM_ACTOR} 2> /dev/null)/python/${LVM_ACTOR}
   export PYTHONPATH=$(ls -1 -d ${LVM_ROOT}/lvm/*/python ${LVM_ROOT}/${LVM_ACTOR}/python 2>/dev/null | tr "\n" ":")
 else
   LVM_ACTOR_PATH=$(${PYTHON} -c "import ${LVM_ACTOR} as _; print(_.__path__[0])")
 fi
-#echo $LVM_ACTOR_PATH
+#echo ${LVM_ACTOR_PATH}
 #echo $PYTHONPATH
 
 export LVMT_DATA_ROOT="${LVM_DATA_ROOT:=/data}"
 
 if [ $LVM_RMQ_HOST ]; then
-  LVM_ACTOR_CONFIG_ABS=$LVM_ACTOR_PATH/etc/${LVM_ACTOR_CONFIG:-${LVM_ACTOR}}_${LVM_RMQ_HOST}
-  sed "s/host: .*$/host: $LVM_RMQ_HOST/" < $LVM_ACTOR_PATH/etc/$LVM_ACTOR_CONFIG.yml \
+  LVM_ACTOR_CONFIG_ABS=${LVM_ACTOR_PATH}/etc/${LVM_ACTOR_CONFIG:-${LVM_ACTOR}}_${LVM_RMQ_HOST}
+  sed "s/host: .*$/host: $LVM_RMQ_HOST/" < ${LVM_ACTOR_PATH}/etc/${LVM_ACTOR_CONFIG}.yml \
             > ${LVM_ACTOR_CONFIG_ABS}.yml
   LVM_ACTOR_CONFIG_ARG="-c ${LVM_ACTOR_CONFIG_ABS}.yml"
-elif  [ $LVM_ACTOR_CONFIG ]; then 
-  LVM_ACTOR_CONFIG_ARG="-c $LVM_ACTOR_PATH/etc/${LVM_ACTOR_CONFIG}.yml"
+elif  [ ${LVM_ACTOR_CONFIG} ]; then 
+  LVM_ACTOR_CONFIG_ARG="-c ${LVM_ACTOR_PATH}/etc/${LVM_ACTOR_CONFIG}.yml"
 fi
 
 
-echo "Using config: $LVM_ACTOR_CONFIG_ARG"
+echo "Using config: ${LVM_ACTOR_CONFIG}_ARG"
 
-${PYTHON} $LVM_ACTOR_PATH/__main__.py ${LVM_ACTOR_CONFIG_ARG} ${LVM_ACTOR_ARGS} start --debug
+${PYTHON} ${LVM_ACTOR_PATH}/__main__.py ${LVM_ACTOR_CONFIG_ARG} ${LVM_ACTOR_ARGS} start --debug
 
-#trap : TERM INT; ${PYTHON} $LVM_ACTOR_PATH/__main__.py ${LVM_ACTOR_CONFIG_ARG} ${LVM_ACTOR_ARGS} start --debug  & wait"]
+#trap : TERM INT; ${PYTHON} ${LVM_ACTOR_PATH}/__main__.py ${LVM_ACTOR_CONFIG_ARG} ${LVM_ACTOR_ARGS} start --debug  & wait"]
 
-if [ $LVM_DEBUG ]; then 
+if [ ${LVM_DEBUG} ]; then 
    sleep INFINITY
 fi
