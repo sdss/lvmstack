d2d7620a8a1960e831e929eead4a6cbacc03a5d9
Florian Briegel <briegel@mpia.de>
2022-08-08 15:11:13 +0200

    added ag pod with real camera

--- config/kube/network/multus.sh
+++ config/kube/network/multus.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+NET_DEV=eno2
+NET_BRIDGE=br-${NET_DEV}
+NET_ADDR=192.168.80
+NET_MASK=24
 
+# If the system wasnt setup on boot with the bridge
+#
+# brctl addbr ${NET_BRIDGE}
+# ip link set ${NET_BRIDGE} up
+# brctl addif ${NET_BRIDGE} ${NET_DEV}
+# ip addr del ${NET_ADDR}.0/${NET_MASK} dev ${NET_DEV}
+# ip addr add ${NET_ADDR}.0/${NET_MASK} dev ${NET_BRIDGE}
 
-
-
-BRIDGE=br0
 
 alias podman-pid="podman inspect --format '{{ .State.Pid }}' $@"
 
 ip link add kube-int type veth peer name kube-ext
 ip link set netns $(podman-pid minikube) dev kube-int
 ip link set kube-ext up
-brctl addif ${BRIDGE} kube-ext
+brctl addif ${NET_BRIDGE} kube-ext
 
 
 kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset-thick-plugin.yml
@@ -30,9 +38,9 @@ spec:
       "mode": "l2",
       "ipam": {
         "type": "host-local",
-        "subnet": "192.168.80.0/24",
-        "rangeStart": "192.168.80.201",
-        "rangeEnd": "192.168.80.225"
+        "subnet": "${NET_ADDR}.0/${NET_MASK}",
+        "rangeStart": "${NET_ADDR}.201",
+        "rangeEnd": "${NET_ADDR}.225"
       }
     }'
 EOF
