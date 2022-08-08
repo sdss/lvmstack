219367ea0b34ca586811cd64ea702a94ab03eba6
Florian Briegel <briegel@mpia.de>
2022-08-08 15:16:32 +0200

    added multus network support

--- config/kube/network/multus.sh
+++ config/kube/network/multus.sh
@@ -5,14 +5,11 @@ NET_BRIDGE=br-${NET_DEV}
 NET_ADDR=192.168.80
 NET_MASK=24
 
-# If the system wasnt setup on boot with the bridge
-#
-# brctl addbr ${NET_BRIDGE}
-# ip link set ${NET_BRIDGE} up
-# brctl addif ${NET_BRIDGE} ${NET_DEV}
-# ip addr del ${NET_ADDR}.0/${NET_MASK} dev ${NET_DEV}
-# ip addr add ${NET_ADDR}.0/${NET_MASK} dev ${NET_BRIDGE}
-
+brctl addbr ${NET_BRIDGE}
+ip link set ${NET_BRIDGE} up
+brctl addif ${NET_BRIDGE} ${NET_DEV}
+ip addr del ${NET_ADDR}.0/${NET_MASK} dev ${NET_DEV}
+ip addr add ${NET_ADDR}.0/${NET_MASK} dev ${NET_BRIDGE}
 
 alias podman-pid="podman inspect --format '{{ .State.Pid }}' $@"
 
@@ -20,7 +17,6 @@ ip link add kube-int type veth peer name kube-ext
 ip link set netns $(podman-pid minikube) dev kube-int
 ip link set kube-ext up
 brctl addif ${NET_BRIDGE} kube-ext
-
 
 kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset-thick-plugin.yml
 
