41e5524e8485fd6ffd42cd510aac93559603f22a
Florian Briegel <briegel@mpia.de>
2022-08-08 16:49:31 +0200

    check on jupyter pyplot for data dim

--- config/jupyter/plotimg.py
+++ config/jupyter/plotimg.py
@@ -10,14 +10,14 @@ def plot_images(data_east, data_west):
     mean, sigma, min, max = np.mean(data_east), np.std(data_east), np.min(data_east), np.max(data_east)
     lperc, uperc = np.percentile(data_east, 5), np.percentile(data_east, 99.95)
 
-    if data_east:
+    if data_east is not None:
         data = data_east if data_east.ndim == 2 else data_east[0]
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,20))
         ax1.set_title("east")
         ax1_im = ax1.imshow(data, vmin=mean-sigma, vmax=uperc)
         fig.colorbar(ax1_im, cax=make_axes_locatable(ax1).append_axes('right', size='3%', pad=0.05), orientation='vertical')
 
-    if data_west:
+    if data_west is not None:
         data = data_west if data_west.ndim == 2 else data_west[0]
         ax2.set_title("west")
         ax2_im = ax2.imshow(data, vmin=mean-sigma, vmax=uperc)
