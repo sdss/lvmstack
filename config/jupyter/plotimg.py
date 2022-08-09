e6901ee14b3563b5e8d319148882b6dcccfc6fab
Florian Briegel <briegel@mpia.de>
2022-08-09 13:24:30 +0200

    removed support for 3-dim imgaes in plot

--- config/jupyter/plotimg.py
+++ config/jupyter/plotimg.py
@@ -15,9 +15,8 @@ def plot_image(data_east):
     lperc, uperc = np.percentile(data_east, 5), np.percentile(data_east, 99.95)
 
     fig, (ax1) = plt.subplots(1, figsize=(16,20))
-    data = data_east if data_east.ndim == 2 else data_east[0]
     ax1.set_title("east")
-    ax1_im = ax1.imshow(data, vmin=mean-sigma, vmax=uperc)
+    ax1_im = ax1.imshow(data_east, vmin=mean-sigma, vmax=uperc)
     fig.colorbar(ax1_im, cax=make_axes_locatable(ax1).append_axes('right', size='3%', pad=0.05), orientation='vertical')
 
 def plot_images(data_east, data_west):
@@ -25,14 +24,12 @@ def plot_images(data_east, data_west):
     lperc, uperc = np.percentile(data_east, 5), np.percentile(data_east, 99.95)
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,20))
-    data = data_east if data_east.ndim == 2 else data_east[0]
     ax1.set_title("east")
-    ax1_im = ax1.imshow(data, vmin=mean-sigma, vmax=uperc)
+    ax1_im = ax1.imshow(data_east, vmin=mean-sigma, vmax=uperc)
     fig.colorbar(ax1_im, cax=make_axes_locatable(ax1).append_axes('right', size='3%', pad=0.05), orientation='vertical')
 
-    data = data_west if data_west.ndim == 2 else data_west[0]
     ax2.set_title("west")
-    ax2_im = ax2.imshow(data, vmin=mean-sigma, vmax=uperc)
+    ax2_im = ax2.imshow(data_west, vmin=mean-sigma, vmax=uperc)
     fig.colorbar(ax2_im, cax=make_axes_locatable(ax2).append_axes('right', size='3%', pad=0.05), orientation='vertical')
     plt.show()
 
