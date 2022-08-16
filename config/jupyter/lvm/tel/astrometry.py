ab6266d22f945d6c86e31f1a054898f4b739080d
Florian Briegel <briegel@mpia.de>
2022-08-16 13:21:05 +0200

    used astropy Angles instead of float

--- config/jupyter/lvm/tel/astrometry.py
+++ config/jupyter/lvm/tel/astrometry.py
@@ -14,6 +14,8 @@ from sdsstools import get_logger
 from math import nan
 from astropy.io import fits
 
+from astropy import units as u
+from astropy.coordinates import SkyCoord, Angle
 
 # TODO: put some real astronomy in here.
 
@@ -50,14 +52,14 @@ async def main():
     parser.add_argument("-v", '--verbose', action='store_true', help="print some notes to stdout")
     parser.add_argument("-t", '--telsubsys', type=str, default="sci", help="Telescope subsystem: sci, skye, skyw or spec")
     parser.add_argument("-e", '--exptime', type=float, default=5.0, help="Expose for for exptime seconds")
-    parser.add_argument("-r", '--ra', help="RA J2000 in hours")
-    parser.add_argument("-d", '--dec', help="DEC J2000 in degrees")
+    parser.add_argument("-r", '--ra', help="RA J2000")
+    parser.add_argument("-d", '--dec', help="DEC J2000")
 
     args = parser.parse_args()
     
     telsubsys = await lvm.from_string(args.telsubsys)
 
-    await Astrometry.calc(telsubsys, args.ra, args.dec, args.exptime, level = DEBUG if args.verbose else INFO)
+    await Astrometry.calc(telsubsys, Angle(args.ra), Angle(args.dec), args.exptime, level = DEBUG if args.verbose else INFO)
 
 
 if __name__ == '__main__':
