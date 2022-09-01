6315133ad78b87e960c7edd21e77e55ad5fd24bc
Florian Briegel <briegel@mpia.de>
2022-09-01 11:55:57 +0200

    fixed missing kube-int up

--- config/kube/network/extraif.sh
+++ config/kube/network/extraif.sh
@@ -2,6 +2,7 @@
 
 . $LVM_ROOT/config/kube/network/multus-config.sh
 
+# optional block, can also be set through 
 brctl addbr ${NET_BRIDGE}
 ip link set ${NET_BRIDGE} up
 brctl addif ${NET_BRIDGE} ${NET_DEV}
@@ -14,5 +15,5 @@ ip link add kube-int type veth peer name kube-ext
 ip link set netns $(podman-pid minikube) dev kube-int
 ip link set kube-ext up
 brctl addif ${NET_BRIDGE} kube-ext
-podman exec  -ti minikube ip link set kube-int
+podman exec  -ti minikube ip link set kube-int up
 
